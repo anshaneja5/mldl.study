dc18c16dd5846c1df6bc8605c99751160680cd06
anshikagarg0410 <garganshika0410@gmail.com>
2025-10-31 22:39:39 +0530

    added more questions to question bank

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ import HomePage from './components/HomePage';
 import Books from './components/Books';
 import Journey from './components/Journey';
 import QuestionBank from './components/QuestionBank';
-import ReactGA from 'react-ga4';
-const trackingId = import.meta.env.VITE_APP_GA_TRACKING_ID;
-ReactGA.initialize(trackingId);
+// import ReactGA from 'react-ga4';
+// const trackingId = import.meta.env.VITE_APP_GA_TRACKING_ID;
+// ReactGA.initialize(trackingId);
 
 const App = () => {
   return (
